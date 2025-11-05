7ddd71354081fd188b08b262eb198ba3dfc25078
rahscripts <rahmantjis@gmail.com>
2025-11-05 22:39:54 +0530

    adding for text and product

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,29 +1,52 @@
 "use client";
 import Image from "next/image";
-
 import { useState } from "react";
+import products from "../products/products";
+
 
 export default function ElonNetWorth() {
   const [amount, setAmount] = useState(490000000000);
 
   const format = (x) => x.toLocaleString('en-US'); // 3-digit groups
 
-  // If you use an input that shows commas:
-  const handleChange = (e) => {
-    const raw = e.target.value.replace(/,/g, "");
-    const num = Number(raw);
-    if (!Number.isNaN(num)) setAmount(num);
-  };
+  const handleOnClick = (item) => {
+    const newAmount = item.price;
+    setAmount(amount-newAmount);
+  }
 
+  // If you use an input that shows commas:
+ 
   return (
+    <>
     <div className="flex flex-col items-center justify-center">
         <Image 
         src={"/elon-funny.webp"}
         width={200}
         height={200}
-        alt="ElonMuskPhoto"/>
-        <h1>Elon Musk's Money</h1>
-      <div>{format(amount)}</div>
+        alt="ElonMuskPhoto"
+        className="rounded m-2 "/>
+        <h1 className="font-semibold">Elon Musk's Money</h1>
+
+      <div className="text-3xl text-green-400 font-bold bg-gray-700 rounded p-1 ">${format(amount)}</div>
+
     </div>
+     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-10">
+      {products.map((item) => (
+        <div key={item.id} onClick={handleOnClick}  className="p-4 border rounded-xl shadow-md cursor-poi">
+          <image
+            src={item.image}
+            alt={item.name}
+            width={300}
+            height={200}
+            className="rounded-lg"
+          />
+          <h2 className="text-xl cursor-pointer font-semibold mt-2">{item.name}</h2>
+          <p className="text-gray-500">
+            ₹{item.price.toLocaleString("en-IN")}
+          </p>
+        </div>
+      ))}
+    </div>
+    </>
   );
 }
---
+++ app/products/products.tsx
@@ -0,0 +1,24 @@
+// src/data/products.js
+
+const products = [
+  {
+    id: 1,
+    name: "Tesla Model S",
+    price: 490000000000,
+    image: "https://www.apple.com/v/macbook-air/specs/a/images/meta/macbook_air_mx__blmrx299u8k2_og.png?202510030406",
+  },
+  {
+    id: 2,
+    name: "iPhone 16 Pro",
+    price: 149000,
+    image: "https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/article/Apple-iPhone-16-Pro-hero-240909_inline.jpg.large.jpg",
+  },
+  {
+    id: 3,
+    name: "MacBook Air M3",
+    price: 134000,
+    image: "https://www.apple.com/v/macbook-air/specs/a/images/meta/macbook_air_mx__blmrx299u8k2_og.png?202510030406",
+  },
+];
+
+export default products;
